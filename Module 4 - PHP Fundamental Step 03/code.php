bfbdbca136b64ec817d60bf8da64d302ec245c4c
Saidul Mursalin Khan <khan_saidul@yahoo.com>
2023-02-20 02:22:20 +0600

    Module 4 - Assignment Updated

--- Module 4 - PHP Fundamental Step 03/code.php
+++ Module 4 - PHP Fundamental Step 03/code.php
@@ -1,19 +1,19 @@
 <?php
 
 // 1st Problem Solution
-// function sortd($a,$b){
-//     return strlen($b)-strlen($a);
-// }
-// $array = array("Hello","I","am","Saidul","Mursalin","Khan");
-// usort($array,'sortd');
-// print_r($array);
+function sortd($a,$b){
+    return strlen($b)-strlen($a);
+}
+$array = array("Hello","I","am","Saidul","Mursalin","Khan");
+usort($array,'sortd');
+print_r($array);
 
 // 2nd Problem Solution
-// function concat($a,$b){
-//     $c = $a.$b;
-//     return $c;
-// }
-// echo concat("Hello","World");
+function concat($a,$b){
+    $c = $a.$b;
+    return $c;
+}
+echo concat("Hello","World");
 
 // 3rd Problem Solution
 function remove($array){
@@ -25,26 +25,26 @@ $array = array("Hello","I","am","Saidul","Mursalin","Khan");
 print_r(remove($array));
 
 // 4th Problem Solution
-// function checkWhiteSpace($string){
-//     if(preg_match('/^[a-zA-Z\s]+$/', $string)){
-//         echo "\n'{$string}' contains only letters and whitespace\n";
-//     }else{
-//         echo "\n'{$string}' contains other characters\n";
-//     }
-// }
-// echo checkWhiteSpace("Hello World");
-// echo checkWhiteSpace("32135143212");
+function checkWhiteSpace($string){
+    if(preg_match('/^[a-zA-Z\s]+$/', $string)){
+        echo "\n'{$string}' contains only letters and whitespace\n";
+    }else{
+        echo "\n'{$string}' contains other characters\n";
+    }
+}
+echo checkWhiteSpace("Hello World");
+echo checkWhiteSpace("32135143212");
 
 // 5th Problem Solution
-// function secondLargest($array){
-//     $max = max($array);
-//     $key = array_search($max,$array);
-//     unset($array[$key]);
-//     $max = max($array);
-//     return $max;
-// }
-// $array = array(22,55,10,2,62,32,12);
-// print_r("Second Largest Value: " . secondLargest($array));
+function secondLargest($array){
+    $max = max($array);
+    $key = array_search($max,$array);
+    unset($array[$key]);
+    $max = max($array);
+    return $max;
+}
+$array = array(22,55,10,2,62,32,12);
+print_r("Second Largest Value: " . secondLargest($array));
 
 
 ?>
