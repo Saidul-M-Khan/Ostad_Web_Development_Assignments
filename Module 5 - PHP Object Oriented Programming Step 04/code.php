d26d8a4cfbca594a79e6e0aef4f187cad74bb326
Saidul Mursalin Khan <khan_saidul@yahoo.com>
2023-02-27 02:15:07 +0600

    Module 5 - PHP Object Oriented Programming Step 04 Assignment Upload

---
+++ Module 5 - PHP Object Oriented Programming Step 04/code.php
@@ -0,0 +1,56 @@
+<!-- Task 1 -->
+<br><br><strong>Task-1</strong><br>
+<form action="<?php 
+echo $_SERVER['PHP_SELF']; 
+?>" method="post">
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name">
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email">
+    <input type="submit" name="submit" id="submit" value="Submit">
+</form>
+
+<?php
+// Task 2
+class Person {
+    private $name, $email;
+    public function __construct( $name, $email ) {
+        $this->name  = $name;
+        $this->email = $email;
+    }
+
+    public function setName( $name ) {
+        $this->name = $name;
+    }
+
+    public function setEmail( $email ) {
+        $this->email = $email;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+}
+$person = new Person( "John", "john@gmail.com" );
+// echo "<br><br><strong>Task-2</strong><br>";
+// echo "Name: " . $person->getName() .   "<br>";
+// echo "Email: " . $person->getEmail() . "<br>";
+
+// Task 3
+if ( isset($_POST['submit']) ) {
+    $name   = $_POST['name'];
+    $email  = $_POST['email'];
+    $person = new Person( $name, $email );
+    $person->setName( $name );
+    $person->setEmail( $email );
+    echo "<br><br><strong>Task-3</strong><br>";
+    echo "Name: " . $person->getName() . "<br>";
+    echo "Email: " . $person->getEmail() . "<br>";
+}
+
+?>
