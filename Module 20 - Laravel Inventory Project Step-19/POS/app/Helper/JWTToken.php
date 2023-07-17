ec1d790d8aa677cc1a37a03b00cfc8d18e15b318
Saidul Mursalin Khan <khan_saidul@yahoo.com>
2023-07-17 22:23:28 +0600

    Module 20 Assignment Done

--- Module 20 - Laravel Inventory Project Step-19/POS/app/Helper/JWTToken.php
+++ Module 20 - Laravel Inventory Project Step-19/POS/app/Helper/JWTToken.php
@@ -42,4 +42,14 @@ class JWTToken {
         return JWT::encode( $payload, $key, 'HS256' );
     }
 
+    public static function VerifyToken( $token ): string {
+        try {
+            $key    = env( 'JWT_KEY' );
+            $decode = JWT::decode( $token, new Key( $key, 'HS256' ) );
+            return $decode->userEmail;
+        } catch ( Exception $e ) {
+            return 'unauthorized';
+        }
+    }
+
 }
